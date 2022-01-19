a62baf534f858461bfc7421e23b67c6d7df2d28e
xman77 <xman77@gmail.com>
2022-01-19 23:32:17 +0800

    53. Index View Book List

--- BookListMVC/wwwroot/js/BookList.js
+++ BookListMVC/wwwroot/js/BookList.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 function loadDataTable() {
     dataTable = $('#DT_load').DataTable({
         "ajax": {
-            "url": "/api/getall",
+            "url": "/books/getall",
             "type": "GET",
             "datatype": "json"
         },
@@ -38,3 +38,28 @@ function loadDataTable() {
     });
 }
 
+function Delete(url) {
+    swal({
+        title: "Are you sure?",
+        text: "Once deleted, you will not be able to recover",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
